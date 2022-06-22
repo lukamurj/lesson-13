b09a93d9c35271184b3a205a4d5e5b716e67c171
lukamurj <lukamurjikneli2006@gmail.com>
2022-06-21 17:07:57 -0700

    add html and javascript pages

---
+++ script.js
@@ -0,0 +1,23 @@
+let userAge = 20,
+  userName = "Nika",
+  isUserOfAge = true,
+  userEmail,
+  userNumber = null;
+
+let myName = "Luka",
+  myHobby = "playing chess",
+  yearOfBirth = 2006,
+  currentYear = 2022;
+console.log(
+  `My name is ${myName}, i am ${
+    currentYear - yearOfBirth
+  } yaers old and my hobby is ${myHobby}`
+);
+console.log(
+  "My name is " +
+    myName +
+    ", i am " +
+    (currentYear - yearOfBirth) +
+    " yaers old and my hobby is " +
+    myHobby
+);
